18f66ffa67913046ec17beb721d5ca3019c64103
Ian Gabriel Paulmino <iangabrielpaulmino1@gmail.com>
2025-09-17 13:28:44 +0800

    chore(deps-dev): add php-stubs/wordpress-stubs for IDE support

---
+++ image-alt-override.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *  Plugin Name: Image Alt Overrides
+ *  Description: Allows per-page alt text overrides for images, improving SEO acceessibility 
+ *  Version: 1.0.0
+ *  Author: Isiah
+ *  Text Domain: image-alt-overrides
+ * 
+ * 
+ */
+
+// Prevents direct access
+if ( ! defined('ABSPATH') ) exit;
+
